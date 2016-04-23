fee0d1dc682f6056af2b3f844c1f3b343045112d
safrinafanfei <feif1021@gmail.com>
2016-04-23 11:57:02 -0700

    Change the name of scroll to scroll_graphics for the fixed graphics visualization. Ready to check in the film strip scroll js function.

--- js/main.js
+++ js/main.js
@@ -9,12 +9,19 @@
 // This function will be executed when the user scrolls the page.
 $(window).scroll(function(e) {
     // Get the position of the location where the scroller starts.
-    var scroller_anchor = $(".scroller_anchor").offset().top;
+      
+  scrollControlGraphics(this);
+  scrollControlFilmStrip(this);
+
+});
+
+function scrollControlGraphics(currentWindow) {
+  var scroller_anchor_graphics = $(".scroller_anchor_graphics").offset().top;
      
     // Check if the user has scrolled and the current position is after the scroller start location and if its not already fixed at the top 
-    if ($(this).scrollTop() >= scroller_anchor && $('.scroller').css('position') != 'fixed') 
+    if ($(currentWindow).scrollTop() >= scroller_anchor_graphics && $('.scroller_graphics').css('position') != 'fixed') 
     {    // Change the CSS of the scroller to hilight it and fix it at the top of the screen.
-        $('.scroller').css({
+        $('.scroller_graphics').css({
             // 'background': '#CCC',
             // 'border': '1px solid #000',
             'position': 'fixed',
@@ -22,18 +29,119 @@ $(window).scroll(function(e) {
             'z-index':99,
             'top': '0px'
         });
+
         // Changing the height of the scroller anchor to that of scroller so that there is no change in the overall height of the page.
-        $('.scroller_anchor').css('height', '50px');
+        $('.scroller_anchor_graphics').css('height', '50px');
     } 
-    else if ($(this).scrollTop() < scroller_anchor && $('.scroller').css('position') != 'relative') 
+    else if ($(currentWindow).scrollTop() < scroller_anchor && $('.scroller').css('position') != 'relative') 
     {    // If the user has scrolled back to the location above the scroller anchor place it back into the content.
          
         // Change the height of the scroller anchor to 0 and now we will be adding the scroller back to the content.
-        $('.scroller_anchor').css('height', '0px');
-         
+        $('.scroller_anchor_graphics').css('height', '0px');
+
         // Change the CSS and put it back to its original position.
-        $('.scroller').css({
+        $('.scroller_graphics').css({
             'position': 'relative'
         });
     }
-});
+
+}
+function scrollControlFilmStrip(currentWindow) {
+
+}
+
+$(document).ready(function(){
+  var subtitles_for_video_1 = [
+    {time:0, caption:"In physics, “like charges repel each other while opposite charges attract,” right?"},
+    {time:5.05, caption:" So why would anyone want to engage in homosexual activity?"},
+    {time:6.19, caption:"Are they out of their minds? Are they psycho? "},
+    {time:9.12, caption:" Isn’t homosexuality inherited? "},
+    {time:12.27, caption:" It’s not an acquired characteristic but an innate trait."},
+    {time:15.09, caption:" I think this is just a normal sexual orientation."},
+    {time:18.24, caption:" I don’t have any issue with homosexuality. "},
+    {time:20.06, caption:" It’s just like, ‘I like drinking beer’ and he likes drinking some <span class=\"highlight\">other beverage.</span> "},
+    {time:23.06, caption:" It’s just like that, very simple."},
+    {time:25.04, caption:" I think it’s okay."},
+    {time:26.09, caption:" If they are good-looking, they will be pleasing to me. "},
+    {time:29.01, caption:" But if they don’t, they nauseate me. "},
+    {time:33.24, caption:" From the perspective of human relationships,"},
+    {time:38.02, caption:" It’s okay if they live together. "},
+    {time:42.16, caption:" But from the perspective of human reproduction,"},
+    {time:46.08, caption:" it’s not gonna work. "},
+    {time:47.07, caption:" It’s absolutely bad!"},
+    {time:48.25, caption:" Homosexuality won’t do any good for society. "},
+    {time:50.07, caption:" Two men living together? "},
+    {time:51.21, caption:" Who does the housework? "},
+    {time:53.07, caption:" Men certainly can’t do housework."},
+    {time:54.19, caption:" Women should do the housework. "},
+    {time:56.08, caption:" Then why would two men bother to get married? "},
+    {time:57.48, caption:" The purpose for getting married is to make a family."},
+    {time:59.27, caption:" Families with one man are already terrible. "},
+    {time:63.01, caption:" Not to mention a family which is has two men."},
+    {time:64, caption:"If it exists, it has a reason."},
+    {time:65.24, caption:" I don’t discriminate them"},
+    {time:66.25, caption:" But I won’t try to be gay myself. "},
+    {time:69.16, caption:" I can’t stand two men being together. "},
+    {time:73.03, caption:" Two dudes making out? "},
+    {time:76.13, caption:" I don’t think it’s appropriate. "},
+    {time:78.21, caption:" Everyone has his own morals and ethics. "},
+    {time:82.04, caption:" I think it’s his own choice. "},
+  ];
+
+ 
+
+  var subtitles_for_video_2 = [
+    {time:4.3, caption:"Something here what they are saying."},
+    {time:5.1, caption:"Something here what they are saying."},
+    {time:8.3, caption:"Something here what they are saying."},
+    {time:41.4, caption:"Something here what they are saying."}
+  ];
+
+  var questions = [
+  "what do you think of homosexuality?",
+  "what do you think of homosexuality?"
+  ];
+  
+   $("#video1")
+      .on("playProgress", function(e, data){
+        for(var i=0; i < subtitles_for_video_1.length; i++){
+          if(data.seconds > subtitles_for_video_1[subtitles_for_video_1.length - 1].time){
+            $("#subtitles").html(subtitles_for_video_1[subtitles_for_video_1.length - 1].caption);
+          } else {
+            if(data.seconds > subtitles_for_video_1[i].time && data.seconds < subtitles_for_video_1[i + 1].time){
+              $("#subtitles").html(subtitles_for_video_1[i].caption);
+            }
+          }
+        }
+      });
+  
+  $("#question1").on("click", function(){
+    $("#filmstrip").stop().animate({"left":0}, {queue:false, duration:600});
+    /* Needs vimeo plugin to work */
+
+    $("#question_title").text(questions[0])
+
+    $("#video1")
+      .vimeo("play");
+
+  });
+
+  $("#question2").on("click", function(){
+    $("#filmstrip").stop().animate({"left":-400}, {queue:false, duration:600});
+    /* Needs vimeo plugin to work */
+    $("#video2")
+      .vimeo("play")
+      .on("playProgress", function(e, data){
+        for(var i=0; i < subtitles_for_video_2.length; i++){
+          if(data.seconds > subtitles_for_video_2[subtitles_for_video_2.length - 1].time){
+            $("#subtitles").html(subtitles_for_video_2[subtitles_for_video_2.length - 1].caption);
+          } else {
+            if(data.seconds > subtitles_for_video_2[i].time && data.seconds < subtitles_for_video_2[i + 1].time){
+              $("#subtitles").html(subtitles_for_video_2[i].caption);
+            }
+          }
+        }
+      });
+  });
+
+})
